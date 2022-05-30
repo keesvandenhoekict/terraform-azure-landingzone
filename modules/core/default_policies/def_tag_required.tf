7c310de286b0e0b2f59200837d58da1a452a27b1
Kees <vandenhoekict@gmail.com>
2022-05-30 14:58:27 +0200

    policy tag req fix

---
+++ modules/core/default_policies/def_tag_required.tf
@@ -0,0 +1,46 @@
+variable "tag_required" {
+  type = any
+  default = {
+    tag_from_list = {
+      display_name = "Tag is Required"
+      description  = "Require the tag to be present"
+      mode         = "Indexed"
+      metadata = {
+        category = "General"
+      }
+      policy_rule = <<RULE
+{
+    "if": {
+            "field": "[concat('tags[', parameters('tagName'), ']')]",
+            "exists": false
+          }
+    "then": {
+      "effect": "[parameters('effect')]"
+    }
+  }
+RULE
+      parameters = {
+
+        tagName = {
+          type          = "string"
+          defaultValue  = null
+          allowedValues = []
+          metadata = {
+            displayName = "Tag Name"
+            description = "Name of the tag to validate"
+          }
+        }
+        effect = {
+          type          = "string"
+          defaultValue  = "Deny"
+          allowedValues = ["Audit", "Deny", "Disabled"]
+          metadata = {
+            displayName = "Effect"
+            description = "The effect determines what happens when the policy rule is evaluated to match"
+          }
+        }
+      }
+      predefined_params = []
+    }
+  }
+}

95d3b4ff8b991b20a934ce861936a247a0b433b2
Kees <vandenhoekict@gmail.com>
2022-09-04 15:26:22 +0200

    ad group

--- modules/core/default_policies/default_policies.tf
+++ modules/core/default_policies/default_policies.tf
@@ -1,8 +1,10 @@
 locals {
   output_policies = {
-    taq_region       = var.tag_region
-    tag_rg_region    = var.tag_rg_region
-    tag_from_list    = var.tag_from_list
-    tag_rg_from_list = var.tag_rg_from_list
+    taq_region            = var.tag_region
+    tag_rg_region         = var.tag_rg_region
+    tag_from_list         = var.tag_from_list
+    tag_rg_from_list      = var.tag_rg_from_list
+    deny_preview_features = var.deny_preview_features
   }
+  output_non_compliance_messages = var.non_compliance_messages
 }
--- modules/core/default_policies/output.tf
+++ modules/core/default_policies/output.tf
@@ -1,3 +1,7 @@
 output "policies" {
   value = local.output_policies
 }
+
+output "non_compliance_messages" {
+  value = local.output_non_compliance_messages
+}
